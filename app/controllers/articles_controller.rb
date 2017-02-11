3f0b7f6cc0f710e53ae75c7bb2a6088d60ed6149
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-02-11 11:50:48 +0000

    implement delete and refactor code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,32 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  
   def index
     @articles = Article.all
   end
   
   def show
-    @article = Article.find(params[:id])
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = "Article has been updated"
       redirect_to @article
     else
       flash.now[:danger] = "Article has not been updated"
       render 'edit'
+    end
+  end
+  
+  def destroy
+    if @article.destroy
+      flash[:success] = "Article has been deleted"
+      redirect_to articles_path
+    else
+      flash.now[:danger] = "Article has not been deleted"
     end
   end
   
@@ -50,6 +58,10 @@ class ArticlesController < ApplicationController
   
     def article_params
       params.require(:article).permit(:title, :body)
+    end
+    
+    def find_article
+      @article = Article.find(params[:id])
     end
   
 end
