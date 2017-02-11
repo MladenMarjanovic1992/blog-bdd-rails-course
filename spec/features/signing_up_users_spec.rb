0b3ba55a6f38dabc4eaf82dd241d87bb86bf5aa1
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-02-11 15:12:41 +0000

    style devise signup view

---
+++ spec/features/signing_up_users_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.feature "Signing up users" do
+  
+  scenario "with valid credentials" do
+    
+    visit "/"
+    click_link "Sign Up"
+    
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    click_button "Sign up"
+    
+    expect(page).to have_content "Welcome! You have signed up successfully."
+    
+  end
+  
+  scenario "with valid credentials" do
+    
+    visit "/"
+    click_link "Sign Up"
+    
+    fill_in "Email", with: "mladen@mladen.com"
+    fill_in "Password", with: "zvezdara92"
+    fill_in "Password confirmation", with: "zvezdara92"
+    click_button "Sign up"
+    
+    #expect(page).to have_content "You have not signed up successfully."
+    
+  end
+    
+end
