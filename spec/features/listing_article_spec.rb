feb4dec9d5db2a51ee1928dce1a93d938a907655
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-02-20 20:04:10 +0000

    update spec to verify owner sees edit and delete

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,9 +3,21 @@ require "rails_helper"
 RSpec.feature "Listing Articles" do
   
   before do
-    john = User.create!(email: "john@example.com", password: "password")
-    @article1 = Article.create(title: "First", body: "Lorem ipsum dolor", user: john)
-    @article2 = Article.create(title: "Second", body: "Lorem ipsum dolor second", user: john)
+    @john = User.create!(email: "john@example.com", password: "password")
+    @article1 = Article.create(title: "First", body: "Lorem ipsum dolor", user: @john)
+    @article2 = Article.create(title: "Second", body: "Lorem ipsum dolor second", user: @john)
+  end
+  
+  scenario "with articles created and user not signed in" do
+    visit "/"
+    
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
   end
   
   scenario "A user lists all articles" do
@@ -17,6 +29,19 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+  end
+  
+  scenario "with articles created and user signed in" do
+    login_as(@john)
+    visit "/"
+    
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
   end
   
   scenario "A user has no articles" do
